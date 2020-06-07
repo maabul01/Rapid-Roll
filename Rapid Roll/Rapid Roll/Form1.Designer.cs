7642d470265a4550898402b0357973ed3d7e93c6
Maabul Quddus <39990322+maabul01@users.noreply.github.com>
2020-06-07 14:13:28 +0530

    Initial Commit
    A simple Rapid Roll game
    
    Ideas:
    Adding Themes

---
+++ Rapid Roll/Rapid Roll/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Rapid_Roll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Wire = new System.Windows.Forms.PictureBox();
+            this.Life1 = new System.Windows.Forms.PictureBox();
+            this.Life2 = new System.Windows.Forms.PictureBox();
+            this.Life3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Controller = new System.Windows.Forms.Timer(this.components);
+            this.Generate = new System.Windows.Forms.Timer(this.components);
+            this.Detect = new System.Windows.Forms.Timer(this.components);
+            this.Observe = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Wire)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Life1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Life2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Life3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Wire
+            // 
+            this.Wire.BackgroundImage = global::Rapid_Roll.Properties.Resources.wire;
+            this.Wire.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Wire.Location = new System.Drawing.Point(0, 0);
+            this.Wire.Name = "Wire";
+            this.Wire.Size = new System.Drawing.Size(385, 15);
+            this.Wire.TabIndex = 0;
+            this.Wire.TabStop = false;
+            // 
+            // Life1
+            // 
+            this.Life1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Life1.BackgroundImage")));
+            this.Life1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Life1.Location = new System.Drawing.Point(12, 21);
+            this.Life1.Name = "Life1";
+            this.Life1.Size = new System.Drawing.Size(25, 25);
+            this.Life1.TabIndex = 1;
+            this.Life1.TabStop = false;
+            // 
+            // Life2
+            // 
+            this.Life2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Life2.BackgroundImage")));
+            this.Life2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Life2.Location = new System.Drawing.Point(43, 21);
+            this.Life2.Name = "Life2";
+            this.Life2.Size = new System.Drawing.Size(25, 25);
+            this.Life2.TabIndex = 2;
+            this.Life2.TabStop = false;
+            // 
+            // Life3
+            // 
+            this.Life3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Life3.BackgroundImage")));
+            this.Life3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Life3.Location = new System.Drawing.Point(74, 21);
+            this.Life3.Name = "Life3";
+            this.Life3.Size = new System.Drawing.Size(25, 25);
+            this.Life3.TabIndex = 3;
+            this.Life3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(277, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Score: 0";
+            // 
+            // Ball
+            // 
+            this.Ball.Location = new System.Drawing.Point(169, 171);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(32, 32);
+            this.Ball.TabIndex = 5;
+            this.Ball.TabStop = false;
+            this.Ball.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawBall);
+            // 
+            // Controller
+            // 
+            this.Controller.Enabled = true;
+            this.Controller.Interval = 1;
+            this.Controller.Tick += new System.EventHandler(this.Controlling);
+            // 
+            // Generate
+            // 
+            this.Generate.Enabled = true;
+            this.Generate.Interval = 350;
+            this.Generate.Tick += new System.EventHandler(this.Generator);
+            // 
+            // Detect
+            // 
+            this.Detect.Enabled = true;
+            this.Detect.Interval = 1;
+            this.Detect.Tick += new System.EventHandler(this.Detector);
+            // 
+            // Observe
+            // 
+            this.Observe.Enabled = true;
+            this.Observe.Interval = 1;
+            this.Observe.Tick += new System.EventHandler(this.Observer);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Life3);
+            this.Controls.Add(this.Life2);
+            this.Controls.Add(this.Life1);
+            this.Controls.Add(this.Wire);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Rapid Roll";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Pressed);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Released);
+            ((System.ComponentModel.ISupportInitialize)(this.Wire)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Life1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Life2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Life3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Wire;
+        private System.Windows.Forms.PictureBox Life1;
+        private System.Windows.Forms.PictureBox Life2;
+        private System.Windows.Forms.PictureBox Life3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer Controller;
+        private System.Windows.Forms.Timer Generate;
+        private System.Windows.Forms.Timer Detect;
+        private System.Windows.Forms.Timer Observe;
+    }
+}
+
---
+++ Rapid Roll/Rapid Roll/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rapid_Roll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Interface();
+        }
+
+        const int width = 90;
+        const int height = 15;
+
+        int save = 0;
+        int count = 0;
+        int lives = 3;
+
+        short score = 0;
+
+        bool moveLeft = false;
+        bool moveRight = false;
+
+        Random r = new Random();
+
+        private void DrawBall(object sender, PaintEventArgs e)
+        {
+            Rectangle ball = new Rectangle(0, 0, 30, 30);
+            Pen pn = new Pen(Color.Black);
+            SolidBrush brush = new SolidBrush(Color.Red);
+            e.Graphics.DrawEllipse(pn, ball);
+            e.Graphics.FillEllipse(brush, ball);
+        }
+        private void Pressed(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                moveLeft = true;
+            }
+            else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                moveRight = true;
+            }
+        }
+        private void Released(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                moveLeft = false;
+            }
+            else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                moveRight = false;
+            }
+        }
+        private void Controlling(object sender, EventArgs e)
+        {
+            if (moveLeft && Ball.Location.X >= 0)
+            {
+                Ball.Left -= 6;
+            }
+            else if (moveRight && Ball.Location.X <= ClientSize.Width - Ball.Width)
+            {
+                Ball.Left += 6;
+            }
+        }
+        private void SetBall()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is PictureBox && c.Name == "Layer")
+                {
+                    PictureBox layer = (PictureBox)c;
+                    Ball.Location = new Point(layer.Location.X, layer.Location.Y - Ball.Height - 5);
+                }
+            }
+        }
+        private void Interface()
+        {
+            int x, c, y = 0;
+
+            while (y < ClientSize.Height - Ball.Height)
+            {
+                x = r.Next(0, ClientSize.Width - width);
+                c = r.Next(60, 120);
+                y += c;
+                if (y > ClientSize.Height - Ball.Height) break;
+                Bar(x, y);
+            }
+
+            SetBall();
+        }
+        private void Bar(int w, int h)
+        {
+            PictureBox bar = new PictureBox();
+            bar.Location = new Point(w, h);
+            bar.Size = new Size(width, height);
+            bar.BackgroundImage = Properties.Resources.brick;
+            bar.BackgroundImageLayout = ImageLayout.Stretch;
+            bar.AccessibleName = "Field";
+            bar.Name = "Bar";
+            this.Controls.Add(bar);
+            Layer(bar);
+        }
+        private void Layer(PictureBox b)
+        {
+            PictureBox layer = new PictureBox();
+            layer.Location = new Point(b.Location.X, b.Location.Y - 5);
+            layer.Size = new Size(width, 1);
+            layer.AccessibleName = "Field";
+            layer.Name = "Layer";
+            this.Controls.Add(layer);
+        }
+        private void Spike(int w, int h)
+        {
+            PictureBox spike = new PictureBox();
+            spike.Location = new Point(w, h);
+            spike.Size = new Size(width, height);
+            spike.BackgroundImage = Properties.Resources.spikes;
+            spike.BackgroundImageLayout = ImageLayout.Stretch;
+            spike.AccessibleName = "Field";
+            spike.Name = "Spike";
+            this.Controls.Add(spike);
+        }
+        private void Generator(object sender, EventArgs e)
+        {
+            if (save == 0)
+            {
+                int pick = r.Next(1, 5) + 1;
+                save = pick;
+            }
+
+            count++;
+
+            if (count == save)
+            {
+                Spike(r.Next(ClientSize.Width - width), ClientSize.Height);
+                count = 0; save = 0;
+            }
+            else
+            {
+                Bar(r.Next(ClientSize.Width - width), ClientSize.Height);
+            }
+        }
+        private void Detector(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is PictureBox && c.AccessibleName == "Field")
+                {
+                    PictureBox field = (PictureBox)c;
+                    field.Top -= 3;
+
+                    if (field.Bounds.IntersectsWith(Wire.Bounds))
+                    {
+                        this.Controls.Remove(field);
+                    }
+                }
+            }
+        }
+        private bool Settled(PictureBox surface)
+        {
+            return surface.Location.Y - Ball.Location.Y >= Ball.Height - 6;
+        }
+        private void Observer(object sender, EventArgs e)
+        {
+            int top = 3;
+
+            if (Ball.Bounds.IntersectsWith(Wire.Bounds) || Ball.Location.Y >= ClientSize.Height)
+            {
+                checkStatus();
+                return;
+            }
+            foreach (Control c in this.Controls)
+            {
+                if (c is PictureBox && c.Name == "Layer")
+                {
+                    PictureBox layer = (PictureBox)c; 
+
+                    if (Ball.Bounds.IntersectsWith(layer.Bounds) && Settled(layer))
+                    {
+                        top *= (-1);
+                    }
+                }
+                else if (c is PictureBox && c.Name == "Spike")
+                {
+                    PictureBox spike = (PictureBox)c; 
+
+                    if (Ball.Bounds.IntersectsWith(spike.Bounds))
+                    {
+                        checkStatus();
+                        return;
+                    }
+                }
+            }         
+            MoveBall(top); 
+            WriteScore(top); 
+            CheckForWinner();
+        }
+        private void MoveBall(int num)
+        {
+            Ball.Location = new Point(Ball.Location.X, Ball.Location.Y + num);
+        }
+        private void checkStatus()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is PictureBox && c.Name.Contains("Life"))
+                {
+                    this.Controls.Remove(c);
+                    lives--;
+                    break;
+                }
+            }
+
+            if (lives == 0)
+            {
+                DisposeTimers();
+                MessageBox.Show("Game Over!" + "\n"
+                              + "Score: " + score.ToString()
+                               );
+            }
+            else
+            {
+                SetBall();
+            }
+        }
+        private void CheckForWinner()
+        {
+            if (score == short.MaxValue)
+            {
+                DisposeTimers();
+                MessageBox.Show("Congratulations, you won!");
+            }
+        }
+        private void WriteScore(int value)
+        {
+            if (value > 0)
+            {
+                score++;
+                label1.Text = "Score: " + score.ToString();
+            }
+        }
+        private void DisposeTimers()
+        {
+            Controller.Dispose();
+            Generate.Dispose();
+            Detect.Dispose();
+            Observe.Dispose();
+        }
+    }
+}
